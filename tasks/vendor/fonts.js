26eb133041db9ea0ee683492ae0bd84d19bd91e1
Sjors van Dongen <MrAtiebatie@users.noreply.github.com>
2016-07-28 14:21:11 +0200

    Update fonts.js

--- tasks/vendor/fonts.js
+++ tasks/vendor/fonts.js
@@ -1,6 +1,10 @@
 module.exports = function (gulp, plugins, settings) {
 
-    gulp.src(settings.bower + '/**/fonts/*.{ttf,woff,woff2,eot,eof,svg}')
-        .pipe(plugins.flatten())
-        .pipe(gulp.dest(settings.dest + 'fonts'));
+    return function (gulp, plugins, settings) {
+
+        gulp.src(settings.bower + '/**/fonts/*.{ttf,woff,woff2,eot,eof,svg}')
+            .pipe(plugins.flatten())
+            .pipe(gulp.dest(settings.dest + 'fonts'));
+
+    }.bind(this, gulp, plugins, settings);
 }
