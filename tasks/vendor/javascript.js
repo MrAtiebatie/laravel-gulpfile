5599d365ae82825fc3b26d3f8f5c2457b0e97ac9
Sjors van Dongen <MrAtiebatie@users.noreply.github.com>
2016-07-28 14:21:22 +0200

    Update javascript.js

--- tasks/vendor/javascript.js
+++ tasks/vendor/javascript.js
@@ -1,10 +1,14 @@
 module.exports = function (gulp, plugins, settings) {
 
-    var jsFiles = plugins.mainBowerFiles({ filter: new RegExp('.*js$', 'i'), checkExistence: true });
-    console.log('JavaScript files', jsFiles, "\n");
+    return function (gulp, plugins, settings) {
 
-    gulp.src(jsFiles)
-       .pipe(plugins.concat('vendor.js'))
-       .pipe(plugins.uglify())
-       .pipe(gulp.dest(settings.dest + 'js'));
+        var jsFiles = plugins.mainBowerFiles({ filter: new RegExp('.*js$', 'i'), checkExistence: true });
+        console.log('JavaScript files', jsFiles, "\n");
+
+        gulp.src(jsFiles)
+        .pipe(plugins.concat('vendor.js'))
+        .pipe(plugins.uglify())
+        .pipe(gulp.dest(settings.dest + 'js'));
+
+    }.bind(this, gulp, plugins, settings);
 }
