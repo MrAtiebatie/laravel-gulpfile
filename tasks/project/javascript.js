15755181e4f98e7217deedb60d5bb08cc410532c
Sjors van Dongen <MrAtiebatie@users.noreply.github.com>
2016-07-28 16:38:02 +0200

    Update javascript.js
    wrapped in function

--- tasks/project/javascript.js
+++ tasks/project/javascript.js
@@ -1,15 +1,19 @@
 module.exports = function (gulp, plugins, settings) {
 
-    plugins.webpack(require(settings.root + '/' + settings.webpack), function(err, stats) {
-        if (err || stats.compilation.missingDependencies.length > 0 || stats.compilation.errors.length > 0) {
-            // Send error notification
-            settings.error([err, stats.compilation.missingDependencies, stats.compilation.errors[0].message]);
-            return;
-        }
+    return function (gulp, plugins, settings) {
 
-        plugins.notifier.notify({
-            message: "Webpack has finished",
-            icon: "node_modules/laravel-elixir/icons/laravel.png"
+        plugins.webpack(require(settings.root + '/' + settings.webpack), function(err, stats) {
+            if (err || stats.compilation.missingDependencies.length > 0 || stats.compilation.errors.length > 0) {
+                // Send error notification
+                settings.error([err, stats.compilation.missingDependencies, stats.compilation.errors[0].message]);
+                return;
+            }
+
+            plugins.notifier.notify({
+                message: "Webpack has finished",
+                icon: "node_modules/laravel-elixir/icons/laravel.png"
+            });
         });
-    });
+
+    }.bind(this, gulp, plugins, settings);
 }
