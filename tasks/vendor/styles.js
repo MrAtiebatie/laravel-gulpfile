6c042208ff89d4ea15e425deade9c2e8d1f0e6ed
Sjors van Dongen <MrAtiebatie@users.noreply.github.com>
2016-07-28 14:21:34 +0200

    Update styles.js

--- tasks/vendor/styles.js
+++ tasks/vendor/styles.js
@@ -1,31 +1,35 @@
 module.exports = function (gulp, plugins, settings) {
 
-    // LESS files
-    var lessFiles = plugins.mainBowerFiles({ filter: new RegExp('.*less$', 'i') });
-    console.log('LESS files:', lessFiles, "\n");
+    return function (gulp, plugins, settings) {
 
-    var lessStream = gulp.src(lessFiles)
-        .pipe(plugins.less())
-        .pipe(plugins.concat('less-files.css'));
+        // LESS files
+        var lessFiles = plugins.mainBowerFiles({ filter: new RegExp('.*less$', 'i') });
+        console.log('LESS files:', lessFiles, "\n");
 
-    // SASS files
-    var sassFiles = plugins.mainBowerFiles({ filter: new RegExp('.*scss$', 'i') });
-    console.log('SASS files:', sassFiles, "\n");
+        var lessStream = gulp.src(lessFiles)
+            .pipe(plugins.less())
+            .pipe(plugins.concat('less-files.css'));
 
-    var sassStream = gulp.src(sassFiles)
-        .pipe(plugins.sass())
-        .pipe(plugins.concat('sass-files.css'));
+        // SASS files
+        var sassFiles = plugins.mainBowerFiles({ filter: new RegExp('.*scss$', 'i') });
+        console.log('SASS files:', sassFiles, "\n");
 
-    // CSS files
-    var cssFiles = plugins.mainBowerFiles({ filter: new RegExp('[^\s]+(\.?([^s]css))$', 'i') });
-    console.log('CSS files', cssFiles, "\n");
+        var sassStream = gulp.src(sassFiles)
+            .pipe(plugins.sass())
+            .pipe(plugins.concat('sass-files.css'));
 
-    var cssStream = gulp.src(cssFiles)
-        .pipe(plugins.concat('css-files.css'));
+        // CSS files
+        var cssFiles = plugins.mainBowerFiles({ filter: new RegExp('[^\s]+(\.?([^s]css))$', 'i') });
+        console.log('CSS files', cssFiles, "\n");
 
-    // Concat files
-    plugins.merge(lessStream, sassStream, cssStream)
-        .pipe(plugins.concat('vendor.css'))
-        .pipe(plugins.clean())
-        .pipe(gulp.dest(settings.dest + 'css'));
+        var cssStream = gulp.src(cssFiles)
+            .pipe(plugins.concat('css-files.css'));
+
+        // Concat files
+        plugins.merge(lessStream, sassStream, cssStream)
+            .pipe(plugins.concat('vendor.css'))
+            .pipe(plugins.clean())
+            .pipe(gulp.dest(settings.dest + 'css'));
+
+    }.bind(this, gulp, plugins, settings);
 }
