fb48870b1cb352955bcb39e3ccb296ced006000c
Sjors van Dongen <MrAtiebatie@users.noreply.github.com>
2016-07-28 16:38:16 +0200

    Update less.js
    Wrapped in function

--- tasks/project/less.js
+++ tasks/project/less.js
@@ -1,8 +1,12 @@
 module.exports = function (gulp, plugins, settings) {
 
-    gulp.src(settings.less.compile)
-        .pipe(plugins.less().on('error', settings.error))
-        .pipe(plugins.autoprefixer())
-        .pipe(plugins.concat('app.css'))
-        .pipe(gulp.dest(settings.dest + 'css'));
+    return function (gulp, plugins, settings) {
+
+        gulp.src(settings.less.compile)
+            .pipe(plugins.less().on('error', settings.error))
+            .pipe(plugins.autoprefixer())
+            .pipe(plugins.concat('app.css'))
+            .pipe(gulp.dest(settings.dest + 'css'));
+
+    }.bind(this, gulp, plugins, settings);
 }
