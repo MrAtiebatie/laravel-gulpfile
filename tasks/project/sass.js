5cb380e59257023d5ab76dea4ff2055488bc46e3
Sjors van Dongen <MrAtiebatie@users.noreply.github.com>
2016-07-28 11:47:59 +0200

    Update sass.js
    plugins.

--- tasks/project/sass.js
+++ tasks/project/sass.js
@@ -3,7 +3,7 @@ module.exports = function (gulp, plugins, settings) {
     gulp.src(settings.sass.compile)
         .pipe(plugins.sass().on('error', settings.error))
         .pipe(plugins.autoprefixer())
-        .pipe(concat('app.css'))
-        .pipe(notify('Success!'))
+        .pipe(plugins.concat('app.css'))
+        .pipe(plugins.notify('Success!'))
         .pipe(gulp.dest(settings.dest + 'css'));
 }
